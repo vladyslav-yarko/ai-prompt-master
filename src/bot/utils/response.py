c4b2734a0cb3d80673d674080c73318af6be5425
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-23 02:41:21 +0300

    feat(bot): utils add message response class

--- src/bot/utils/response.py
+++ src/bot/utils/response.py
@@ -15,3 +15,25 @@ class Response(ABC):
     @abstractmethod
     async def answer(self):
         raise NotImplementedError()
+
+
+class MessageResponse(Response):
+    def __init__(
+        self, 
+        message: Message, 
+        text: str = '', 
+        keyboard: Optional[Keyboard] = None, 
+        state: Optional[FSMContext] = None,
+        parse_mode: Optional[str] = "Markdown"):
+        self.message = message
+        self.keyboard = keyboard
+        self.text = text
+        self.state = state
+        self.parse_mode = parse_mode
+
+    async def answer(self):
+        await self.message.answer(
+            text=self.text,
+            reply_markup=self.keyboard,
+            parse_mode=self.parse_mode
+        )
