f50af21b388d5e238f5346f14612e2943cc23621
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-22 16:34:41 +0300

    feat(utils): add llm manager

---
+++ src/utils/llm.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from g4f.client import AsyncClient
+from g4f.Provider import Chatai 
+
+from src.config import settings
+
+
+class LLM:
+    def __init__(
+        self, 
+        model: Optional[None] = None,
+        temperature: float = 0.5):
+        self.client = AsyncClient(
+            provider=Chatai
+        )
+        self.model = model if model is not None else settings.MODEL
+        self.temperature = temperature
+        
+    async def ask(self, messages: list[dict]):
+        response = await self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            temperature=0.5
+        )
+        clean_response = response.choices[0].message.content
+        return clean_response
