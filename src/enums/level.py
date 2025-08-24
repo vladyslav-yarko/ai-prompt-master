852bed46a2ee1536a9fd60ae57ddf95c76212338
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-25 02:45:34 +0300

    feat(bot): enums add level enum

---
+++ src/enums/level.py
@@ -0,0 +1,14 @@
+import enum
+
+
+class LevelEnum(str, enum.Enum):
+    beginner = "beginner"
+    novice = "novice"
+    learner = "learner"
+    junior = "junior"
+    intermediate = "intermediate"
+    skilled = "skilled"
+    advanced = "advanced"
+    proficient = "proficient"
+    expert = "expert"
+    master = "master"
