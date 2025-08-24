6e1e015966b7162f4c2edcb810f88ae0f9e8ae8e
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-25 01:08:55 +0300

    chore(migrations): configure env file

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from src.config import settings
-from src.models import Base
+from src.models import Base, Achievement, Level, user_achievement, User, UserStats, Game
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+from src.models.base import Base
+from src.models.achievement import Achievement
+from src.models.level import Level
+from src.models.user_achievement import UserAchievement
+from src.models.user_stats import UserStats
+from src.models.user import User
+from src.models.game import Game
