82e2c5415c49dcf55c44d1016fce5a2f70c94222
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-27 03:37:47 +0300

    feat(repositories): add import of all repositories

---
+++ src/repositories/__init__.py
@@ -0,0 +1,6 @@
+from src.repositories.achievement import AchievementRepository
+from src.repositories.level import LevelRepository
+from src.repositories.user_achievement import UserAchievementRepository
+from src.repositories.user_stats import UserStatsRepository
+from src.repositories.user import UserRepository
+from src.repositories.game import GameRepository
