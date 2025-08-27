e3c6d23e956de277ff3c4b131dc749ea5a223579
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-27 13:41:07 +0300

    feat(bot): import all middlewares

---
+++ src/bot/middlewares/__init__.py
@@ -0,0 +1,5 @@
+# All middlewares are similar to dependencies like in framework FastAPI
+
+from src.bot.middlewares.user import UserMiddleware
+from src.bot.middlewares.base import BaseMiddleware
+from src.bot.middlewares.game import GameMiddleware
