37c7340c6eee20dac51a6edc680003cb00a9557b
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-27 19:50:26 +0300

    feat(bot): add games hand to game inline keyboards

---
+++ src/bot/keyboards/inline/game.py
@@ -0,0 +1,17 @@
+from src.bot.utils.keyboard import InlineKeyboard
+from src.bot.utils.button import CallbackButton
+
+
+def games_hand_keyboard(game_title: str, mode: str) -> InlineKeyboard:
+    keyboard = InlineKeyboard(
+        [
+            [
+                CallbackButton(text="Грати 🎮", callback=f"start_game_{mode}").button
+            ],
+            [
+                CallbackButton(text="Опис 📝", callback=f"description_game_{game_title}").button,
+                CallbackButton(text="Меню 🏠", callback="quit").button
+            ]
+        ]
+    ).keyboard()
+    return keyboard
