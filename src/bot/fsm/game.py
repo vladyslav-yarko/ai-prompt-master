dfb25a59550e1444f5a0f1e5410aead9f17ac54f
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-27 19:33:19 +0300

    feat(bot): add ActiveGameState to game fsm

--- src/bot/fsm/game.py
+++ src/bot/fsm/game.py
@@ -9,4 +9,20 @@ class GameState(StatesGroup):
     code_mode = State()
     anti_prompt_mode = State()
     puzzles_mode = State()
-
+    
+    
+class ActiveGameState(StatesGroup):    
+    learn_mode = State()
+    learn_waiting = State()
+    
+    creative_mode = State()
+    creative_waiting = State()
+    
+    code_mode = State()
+    code_waiting = State()
+    
+    anti_prompt_mode = State()
+    anti_prompt_waiting = State()
+    
+    puzzles_mode = State()
+    puzzles_waiting = State()
