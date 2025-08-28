10b29eb01f915ea28c80867c5ed206e6c1d1779a
Vladislav-Yarko <vladislavv44444@gmail.com>
2025-08-28 15:35:59 +0300

    feat(bot): add e authorize text

---
+++ src/bot/text/__init__.py
@@ -0,0 +1,3 @@
+from src.bot.text.text import e_authorize_text
+
+# with error authorize text I broke DRY a little bit
---
+++ src/bot/text/text.py
@@ -0,0 +1,8 @@
+from src.bot.utils.text import Text
+
+
+e_authorize_text = Text("""
+❗ Ви ще *не створили акаунт*.
+
+Спершу скористайтесь командою */authorize*
+""")
