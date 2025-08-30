6c3a761d3f6a0ea8290a0868a315c9d8b928a48d
vladyslav-yarko <vladislavv44444@gmail.com>
2025-08-31 01:06:34 +0300

    feat(bot): add start hand method to base message response

--- src/bot/responses/base.py
+++ src/bot/responses/base.py
@@ -1,3 +1,9 @@
 from src.bot.utils.response import MessageResponse, CallbackResponse
 from src.bot.text.base import *
 from src.services import ProgressDataService
+
+
+class BaseMessageResponse(MessageResponse):
+    async def start_hand(self) -> None:
+        self.text = s_start_hand_text.render()
+        await self.answer()
