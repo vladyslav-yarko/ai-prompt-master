8e66f2af08793d46db90dfa3e49ebd9f5719e313
vladyslav-yarko <vladislavv44444@gmail.com>
2025-08-31 01:07:14 +0300

    feat(bot): add help hand method to base message response

--- src/bot/responses/base.py
+++ src/bot/responses/base.py
@@ -7,3 +7,7 @@ class BaseMessageResponse(MessageResponse):
     async def start_hand(self) -> None:
         self.text = s_start_hand_text.render()
         await self.answer()
+
+    async def help_hand(self) -> None:
+        self.text = s_help_hand_text.render()
+        await self.answer()
