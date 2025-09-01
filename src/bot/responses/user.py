e6061ca5ba0e0ce8d0e516877d268cfa8d19ff33
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-02 01:01:14 +0300

    feat(bot): add user response

---
+++ src/bot/responses/user.py
@@ -0,0 +1,6 @@
+from src.bot.utils.response import MessageResponse, CallbackResponse
+from src.bot.utils.markdown import escape_md
+from src.bot.text.user import *
+from src.bot.keyboards.inline.user import delete_command_hand_keyboard
+from src.bot.fsm.user import UserState
+from src.services import UserService
