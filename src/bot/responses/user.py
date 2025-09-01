5158b1946d03728fbe941e86b1f584a98082c9ba
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-02 01:03:26 +0300

    feat(bot): add authorize hand method to user message response class

--- src/bot/responses/user.py
+++ src/bot/responses/user.py
@@ -7,4 +7,14 @@ from src.services import UserService
 
 
 class UserMessageResponse(MessageResponse):
-    pass
+    async def authorize_hand(self, service: UserService) -> None:
+        data = {
+            "telegramId": self.message.from_user.id,
+            "chatId": self.message.chat.id
+        }
+        user = await service.create_one(data)
+        if not user:
+            self.text = e_authorize_hand_text.render()
+        else:
+            self.text = s_authorize_hand_text.render()
+        await self.answer()
