b40481a03d1c7eaf3bee4106df4840e4f488cc52
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-02 01:02:28 +0300

    feat(bot): add user message response class

--- src/bot/responses/user.py
+++ src/bot/responses/user.py
@@ -4,3 +4,7 @@ from src.bot.text.user import *
 from src.bot.keyboards.inline.user import delete_command_hand_keyboard
 from src.bot.fsm.user import UserState
 from src.services import UserService
+
+
+class UserMessageResponse(MessageResponse):
+    pass
