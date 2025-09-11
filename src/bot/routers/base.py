2ed26dcd18cc861e9454e20fe644d1bff5cdf31b
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-11 18:45:03 +0300

    feat(bot): add rules hand handler to base router

--- src/bot/routers/base.py
+++ src/bot/routers/base.py
@@ -44,3 +44,11 @@ async def achievement_hand(message: Message, state: FSMContext, service: Progres
         message=message,
         state=state
     ).achievement_hand(service)
+    
+    
+@router.message(StateFilter(None), Command("rules"))
+async def rules_hand(message: Message, state: FSMContext):
+    await BaseMessageResponse(
+        message=message,
+        state=state
+    ).rules_hand()
