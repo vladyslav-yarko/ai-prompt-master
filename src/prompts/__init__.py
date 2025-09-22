0201f8cad6a0e698af80a7b840f5cc9b5585f9c4
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-22 19:52:40 +0300

    fix(bot): import prompts bug

--- src/prompts/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.prompts.game.check import check_prompt
-from src.prompts.game.learn import start_learn_prompt, task_learn_prompt
-from src.prompts.game.creative import start_creative_prompt, task_creative_prompt
-from src.prompts.game.code import start_code_prompt, task_code_prompt
-from src.prompts.game.anti_prompt import start_anti_prompt_prompt, task_anti_prompt_prompt
-from src.prompts.game.puzzles import start_puzzles_prompt, task_puzzles_prompt
