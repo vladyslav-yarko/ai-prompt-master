3043e69c3d5d2be62a014894414c16c8285e1b69
vladyslav-yarko <vladislavv44444@gmail.com>
2025-09-22 19:28:36 +0300

    fix(bot): base router middleware name issue

--- src/bot/routers/base.py
+++ src/bot/routers/base.py
@@ -3,14 +3,14 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 
-from src.bot.middlewares import BaseMiddleware
+from src.bot.middlewares import BaseRouterMiddleware
 from src.bot.responses import BaseMessageResponse, BaseCallbackResponse
 from src.bot.filters import CallDataEq
 from src.services import ProgressDataService
 
 
 router = Router()
-router.message.middleware(BaseMiddleware())
+router.message.middleware(BaseRouterMiddleware())
 # Callback query does not need the service, so middleware was not applied
 
 
